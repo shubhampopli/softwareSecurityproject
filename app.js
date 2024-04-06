207cc5e175942b56f003d286f5b0cb4a66b8c830
shubhampopli <spopli47@gmail.com>
2024-04-05 22:45:05 -0300

    Add express-session, connect-flash and update views/layouts/boilerplate.ejs, app.js, package.json, package-lock.json; remove validateCampground middleware
    Details:
    - express-session and connect-flash are added for session management and flash messages respectively
    - app.js is updated to include session and flash middleware, and to use them in routes
    - views/layouts/boilerplate.ejs is updated to include flash partial
    - package.json and package-lock.json are updated to include new dependencies
    - validateCampground middleware is removed as it is not being used.

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const { campgroundSchema } = require('./schemas.js');
+const mongoose = require('mongoose');
+const catchAsync = require('./Utils/catchAsync');
+const ExpressError = require('./Utils/ExpressError');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb+srv://shubham:spopliii_67@cluster0.kej5hm5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
+app.engine('ejs', ejsMate)
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+app.get('/campgrounds',catchAsync( async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+}));
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+app.post('/campgrounds', validateCampground,catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  }));
+
+app.get('/campgrounds/:id', async (req, res,) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit',catchAsync( async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground });
+}))
+
+app.put('/campgrounds/:id',validateCampground,catchAsync( async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`)
+}));
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`);
+});
